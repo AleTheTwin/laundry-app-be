5070db63bf6bf9001df4efe7e34cb541f41b61c6
Alejandro Sánchez <alethetwin@icloud.com>
2026-04-23 23:26:17 -0600

    movved generic crud to crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service.js';
-import { GenericCrudModule } from './modules/generic-crud/generic-crud.module.js';
+import { CrudModule } from './modules/crud/crud.module.js';
 import { LogsModule } from './modules/logger/logger.module.js';
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        GenericCrudModule,
         LogsModule,
+        CrudModule,
     ],
     controllers: [],
     providers: [PrismaService],
